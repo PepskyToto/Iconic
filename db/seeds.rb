7fe48aca7725f989c063451c48c28091c1682021
VERNET Jean-Francois <jfvernet@icloud.com>
2023-12-05 14:34:15 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+def random_price
+  rand(50..500)
+end
+
+# Tableau de catégories possibles
+categories = ["robe de mariée", "pantalon", "pull", "costumes"]
+
+# Création de 20 instances de produits
+20.times do
+  Product.create!(
+    user_id: 1, # Remplace 1 par l'ID de l'utilisateur approprié
+    category: categories.sample,
+    size: rand(1..5), # Remplace 5 par le nombre maximum de tailles possible
+    price: random_price,
+    title: Faker::Lorem.words(number: 3).join(' '), # Utilisation de Faker pour générer un titre aléatoire
+    description: Faker::Lorem.sentence,
+    availability: [true, false].sample
+  )
+end
