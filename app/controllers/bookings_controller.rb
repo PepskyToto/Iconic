8ab9f5f49c0fe7aaa1ab4088382f19d104f9e46b
thomas paufert <paufert.thomas@gmail.com>
2023-12-05 12:13:42 +0100

    controlleurs OK

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,50 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new
+    @booking.user_id = params[:user_id]
+    @booking.room_id = params[:room_id]
+    @booking.start_date = params[:start_date]
+    @booking.end_date = params[:end_date]
+    @booking.total_price = params[:total_price]
+
+    if @booking.save
+      redirect_to bookings_url, notice: "Booking created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @booking = Booking.find_by(id: params[:id])
+  end
+
+  def update
+    @booking = Booking.find_by(id: params[:id])
+    @booking.user_id = params[:user_id]
+    @booking.room_id = params[:room_id]
+    @booking.start_date = params[:start_date]
+    @booking.end_date = params[:end_date]
+    @booking.total_price = params[:total_price]
+
+    if @booking.save
+      redirect_to bookings_url, notice: "Booking updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @booking = Booking.find_by(id: params[:id])
+    @booking.destroy
+
+    redirect_to bookings_url, notice: "Booking deleted."
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,60 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+
+    if @product.nil?
+      redirect_to products_url, notice: "Product not found."
+    end
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.description = params[:description]
+    @product.image = params[:image]
+
+    if @product.save
+      redirect_to products_url, notice: "Product created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+
+    if @product.nil?
+      redirect_to products_url, notice: "Product not found."
+    end
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.description = params[:description]
+    @product.image = params[:image]
+
+    if @product.save
+      redirect_to products_url, notice: "Product updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+
+    redirect_to products_url, notice: "Product deleted."
+  end
 end
