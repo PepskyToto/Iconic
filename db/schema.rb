4f6cd4283a60d03daad31addb55d61c668fe3f8b
Aurore Bouzac <auroresb@hotmail.fr>
2023-12-06 16:04:30 +0100

    homepage + favorites

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_06_125821) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_06_150317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_06_125821) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
-    t.string "image_url"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
