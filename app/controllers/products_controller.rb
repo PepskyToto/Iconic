6ada42aafe1493a6b6667f154dcf284261fc9210
thomas paufert <paufert.thomas@gmail.com>
2023-12-07 18:25:32 +0100

    En cours pour les filtres backend

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,7 +64,7 @@ class ProductsController < ApplicationController
   private
 
   def set_params
-    params.require(:product).permit(:title, :category, :size, :price, :description, :availability, :user_id)
+    params.require(:product).permit(:title, :category, :size, :price, :description, :availability, :user_id, :couleurs, :sexe, :textile_type,)
   end
 
 end
