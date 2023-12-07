32caacbfecdf545ac289f466d023396c4e32eb8e
VERNET Jean-Francois <jfvernet@icloud.com>
2023-12-07 14:45:58 +0100

    reservation impossible si la date de fin est antérieur à la date de début && si la date de début est antérieur à la date de demain

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,12 +9,18 @@ class BookingsController < ApplicationController
   end
 
   def create
+    product_id = params["booking"][:product_id]
     @booking = Booking.new(set_params)
     @booking.user_id = current_user.id
+    start_date= params["booking"][:start_date]
+    end_date= params["booking"][:end_date]
     #@booking.product_id = params["booking"][:product_id]
-
-
-    if @booking.save
+    start_date_comparision = Date.parse(start_date)
+    if end_date < start_date
+      redirect_to product_path(product_id), notice: "End date must be after start date."
+    elsif start_date_comparision < Date.today
+      redirect_to product_path(product_id), notice: "Start date must be after today."
+    elsif @booking.save
       redirect_to root_path, notice: "Booking created successfully."
     else
       redirect_to new_user_session_path, alert: "Booking creation failed. Please log in."
